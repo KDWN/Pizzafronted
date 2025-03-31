294ebbba0f18bdf9c442820844bed5edb645c66c
KDWN <keiran.downsgts@gmail.com>
2025-04-01 10:02:39 +1300

    Order test 2

--- global.js
+++ global.js
@@ -1,12 +1,20 @@
-export function defpizzas() {
-//  name : price
-    var pizzas = {
+export const pizzas = {
+    //  name : price
         "cheese" : 5,
         "pepperoni" : 8,
         "garlic cheese" : 7.5,
-        "haiwaian" : 10,
+        "haiiwaian" : 10,
         "margherita" : 10,
         "meat lovers" : 12.5
-    };
-    return pizzas;
 };
+
+var debug = false
+window.debug = debug
+
+export function getpizzas() {
+    return pizzas;
+}
+
+export var orderList = [
+    
+]
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import { defpizzas } from "./global";
-
-//var menuitems = document.getElementById("cheese")
-
-//menuitems.addEventListener("click", () => checkPurchase("cheese"))
-
+import { getpizzas } from "./global.js";
 
 export function checkPurchase(itemName){ // Check what and how many products the user wants to purchase.
-    
-    var item = document.getElementsByClassName("menu")
-    var itemCount = prompt("How many " + itemName + " pizzas do you want to purchase?");
+    let itemCount = prompt(`How many ${itemName} pizzas do you want to purchase?`);
     if( itemCount == null || itemCount == "") {return} // checks if input is empty
     while(isNaN(itemCount) || itemCount % 1 != 0) {
         alert("Please input a whole number");
-        itemCount = prompt("How many " + itemName + " pizzas do you want to purchase?");
+        itemCount = prompt(`How many ${itemName} pizzas do you want to purchase?`);
     }
-    confirm("This will cost $" + itemCount + " are you sure you want to purchase");
-    alert(defpizzas())
+    confirm(`This will cost $${itemCount * getpizzas()[itemName.toLowerCase()]}. Are you sure you want to purchase?`);
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".menuitem").forEach(x => {
+        if(debug){console.log("adding lister")};
+        x.addEventListener("click", function() {
+            let itemName = this.querySelector("h1").textContent;
+            if(debug){console.log("clicking")};
+            if(debug){debug}
+            checkPurchase(itemName);
+        });
+    });
+});
